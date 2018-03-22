0feb5013bb828f37dd265a170f5d6841253b87e4
ArakiMasashi <masa121codbf@gmail.com>
2018-03-22 14:10:14 +0900

    Practice05を全て完了

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -23,16 +23,43 @@ public class PTra05_04 {
 		 */
 
 		// ①
-		System.out.println("0～6の整数を入力してください");
+		boolean state = true;
 
-		String line = scanner.nextLine();
+		while(state) {
+			System.out.println("0～6の整数を入力してください");
+			String line = scanner.nextLine();
 
-		switch(line) {
-			case "0":
-				System.out.println("日曜");
-				break;
-			case "1":
-				System.out.println("月曜");
+			switch(line) {
+				case "0":
+					System.out.println("日曜");
+					state = false;
+					break;
+				case "1":
+					System.out.println("月曜");
+					state = false;
+					break;
+				case "2":
+					System.out.println("火曜");
+					state = false;
+					break;
+				case "3":
+					System.out.println("水曜");
+					state = false;
+					break;
+				case "4":
+					System.out.println("木曜");
+					state = false;
+					break;
+				case "5":
+					System.out.println("金曜");
+					state = false;
+					break;
+				case "6":
+					System.out.println("土曜");
+					state = false;
+					break;
+				default:
+			}
 		}
 	}
 }
