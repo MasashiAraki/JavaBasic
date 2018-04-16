637d2e0c0d842205a47b5244cf501e61ac8eda8d
ArakiMasashi <masa121codbf@gmail.com>
2018-04-16 13:56:26 +0900

    Practice16

--- Practice/src/practice16/PTra16_01.java
+++ Practice/src/practice16/PTra16_01.java
@@ -1,5 +1,8 @@
 package practice16;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /*
  */
 public class PTra16_01 {
@@ -8,5 +11,13 @@ public class PTra16_01 {
         /*
         * ★ 現在時刻を「YYYY年MM月DD日」形式で出力してください
         */
+
+//    	Data型は日付を取得
+    	Date date = new Date();
+//    	SimpleDateFormatでフォーマットを設定
+    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+//    	format()で日付を文字列に変換し、変数dateStr(String型)に代入
+    	String dateStr = sdf.format(date);
+    	System.out.println(dateStr);
     }
 }
